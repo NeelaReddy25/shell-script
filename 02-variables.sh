79a99c1d7c52b1af837e9fdd2fac289c6b540a96
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-16 22:53:21 +0530

    some-message

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,4 +3,4 @@
 echo "Ramesh:: Hello Suresh, How are you?"
 echo "Suresh:: Hi Rameh, I am fine. How are you?"
 echo "Ramesh:: I am fine too. how is your work?"
-ech0 "Suresh:: not bad. I am thinking to upgrade to Devops"
+echo "Suresh:: not bad. I am thinking to upgrade to Devops"
