015014b71fffe49015b877966d13a91931f6e62f
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-16 23:01:37 +0530

    some-message

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Ramesh #no space between = and value
 PERSON2=Suresh
 
-echo "Ramesh:: Hello Suresh, How are you?"
-echo "Suresh:: Hi Rameh, I am fine. How are you?"
-echo "Ramesh:: I am fine too. how is your work?"
-echo "Suresh:: not bad. I am thinking to upgrade to Devops"
+echo "PERSON1:: Hello PERSON2, How are you?"
+echo "PERSON2:: Hi PERSON1, I am fine. How are you?"
+echo "PERSON1:: I am fine too. how is your work?"
+echo "PERSON2:: not bad. I am thinking to upgrade to Devops"
