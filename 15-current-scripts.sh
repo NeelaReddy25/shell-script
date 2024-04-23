1f3a20c2d0c73665253cecf5f040c33bea1341a5
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-23 18:53:38 +0530

    Do good and take better

--- 15-current-scripts.sh
+++ 15-current-scripts.sh
@@ -5,8 +5,8 @@ COURSE="Devops from current script."
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-./16-other-scripts.sh
+#./16-other-scripts.sh
 
-#source ./16-other-scripts.sh
+source ./16-other-scripts.sh
 
 echo "After calling other script, course: $COURSE"
