236c2bff5a42088074535590eaa768ed01c45346
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-25 00:41:30 +0530

    some-message

--- 20-backup-archive.sh
+++ 20-backup-archive.sh
@@ -2,7 +2,7 @@
 
 # Defining variables
 SOURCE_DIRECTORY=/tmp/applog-files
-DESTINATION_DIRECTORY=/backups
+DESTINATION_DIRECTORY=/backup
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TARFILE="backup_$SCRIPT_NAME-$TIMESTAMP.tar.gz"
