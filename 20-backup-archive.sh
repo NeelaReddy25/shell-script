0931b030b4c75e29ce470632b080ab218e022229
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-25 01:12:32 +0530

    some-message

--- 20-backup-archive.sh
+++ 20-backup-archive.sh
@@ -12,23 +12,15 @@ N="\e[0m"
 
 mkdir -p "$DESTINATION_DIRECTORY/$TIMESTAMP"
 
-if [ -d $DESTINATION_DIRECTORY ]
-then
-    echo -e "$G Destination directory exists $N"
-else
-    echo -e "$R Please make sure $DESTINATION_DIRECTORY exists $N"
-    exit 1
-fi    
-
 # Archive the source diretory
 tar -czf "$DESTINATION_DIRECTORY/$TIMESTAMP/$TARFILE" "$SOURCE_DIRECTORY"
 
 
 if [ $? -eq 0 ]
 then 
-    echo -e "$R Backup Failure: $TARFILE $N"
+    echo -e "$G Backup Success: $TARFILE $N"
 else
-    echo -e "$G Backup Success: $TARFILE $N" 
+    echo -e "$R Backup Failure: $TARFILE $N" 
 fi       
 
 
