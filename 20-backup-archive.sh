4f5f3480d648ee552cb6b35b617d35c2ac173095
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-25 01:40:33 +0530

    some-message

--- 20-backup-archive.sh
+++ 20-backup-archive.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
-# Defining variables
-SOURCE_DIRECTORY="/tmp"
-DESTINATION_DIRECTORY="/backups"
+
+SOURCE_DIRECTORY=$1
+DESTINATION_DIRECTORY=$2
 TIMESTAMP=$(date +%F-%H-%M-%S)
-TARFILE="backup_$TIMESTAMP.tar.gz"
+BACKUP_FILENAME="${DESTINATION_DIRECTORY}/backup_${TIMESTAMP}.tar.gz"
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-mkdir -p "$DESTINATION_DIRECTORY/$TIMESTAMP"
 
-# Archive the source diretory
-tar -czf "$DESTINATION_DIRECTORY/$TIMESTAMP/$TARFILE" "$SOURCE_DIRECTORY"
-
-
-if [ $? -eq 0 ]
-then 
-    echo -e "$G Backup Success: $TARFILE $N"
+if [ -d SOURCE_DIRECTORY ] 
+then
+     echo -e "$G Source directory exists $N"
 else
-    echo -e "$R Backup Failure: $TARFILE $N" 
-fi       
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+    exit 1
+fi
+
+
+if [ -d DESTINATION_DIRECTORY ] 
+then
+    mkdir -p "$DESTINATION_DIRECTORY"
+
+fi
+
+tar -czvf "$BACKUP_FILENAME" "$SOURCE_DIRECTORY"
+
+
+if [ $? -eq 0 ] 
+then
+    echo -e "Backup ....$G SUCCESS. Archive saved as: $backup_filename $N"
+else
+    echo -e "Backup....$R FAILURE $N"
+fi
+
 
 
 
