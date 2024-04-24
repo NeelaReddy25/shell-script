6729e289627c7ca749f262a8d32e263c5534058d
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-25 02:06:13 +0530

    some-message

--- 20-backup-archive.sh
+++ 20-backup-archive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-SOURCE_DIRECTORY=/tmp/folder
+SOURCE_DIRECTORY=/folder
 DESTINATION_DIRECTORY=/backups
 TIMESTAMP=$(date +%F-%H-%M-%S)
 BACKUP_FILENAME="${DESTINATION_DIRECTORY}/backup_${TIMESTAMP}.tar.gz"
