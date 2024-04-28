8a69980854038b21c98e0f5d8e1b4ccf764863a0
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-29 01:10:02 +0530

    Doing good forever

--- 24-cpu-usages.sh
+++ 24-cpu-usages.sh
@@ -14,12 +14,14 @@ check_cpu_usage() {
   ps -eo pid,ppid,cmd,%cpu,%mem --sort=-%cpu | head -n 6 | tail -n +5 | while read -r pid ppid cmd cpu mem ; do
     # Check if CPU usage of the process is greater than the threshold
     cpu_usage=$(echo "$cpu" | awk '{print int($1+2)}')
+    folder=$(echo "$cpu" | awk '{print int($1+n)}')
     if (( $cpu_usage > $THRESHOLD )); then
-     echo "Process with PID $pid is using $cpu% CPU, which is above the threshold of $THRESHOLD%" | mail -s "High CPU Usage Alert" neelareddy.i10204@gmail.com
+      MESSAGE+="$folder is more than $THERSHOLD, Current usage: $cpu_usage \n"
     fi
   done 
 }
 
+echo "Process with PID $pid is using $cpu% CPU, which is above the threshold of $THRESHOLD%" | mail -s "High CPU Usage Alert" neelareddy.i10204@gmail.com
 
 # Call the function
 check_cpu_usage
