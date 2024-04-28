8736f5dc5ac233a3d200f55ef024de0b5bbda7d4
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-29 01:14:21 +0530

    Doing good forever

--- 23-cpu-usage.sh
+++ 23-cpu-usage.sh
@@ -1,14 +1,32 @@
-#!/bin/bash
+PATH="/"
+HOSTNAME=$(hostname)
+CRITICAL=80
+WARNING=75
+CRITICALMAIL="neelareddy.i25@gmail.com"
+WARNINGMAIL="neelareddy.i10204@gmail.com"
+mkdir -d /var/log/cputilhist
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE=/var/log/cputilhist/cpusage-$TIMESTAMP.log
 
-# Set the threshold (e.g., 80%)
-THRESHOLD=80
+touch $LOGFILE
 
-# Get the current CPU utilization
-CPU_UTILIZATION=$(cat /proc/loadavg | awk '{print $1}')
-
-# Compare with the threshold
-if (( $(echo "$CPU_UTILIZATION > $THRESHOLD" | bc -l) ))
+for path in $PATH
+do 
+    CPULOAD=`top -b -n 5 -d1 | grep "Cpu(s)" | tail -n1 | awk '{print $2}' | awk -F . '{print $1}'`
+if [ -n $WARNING -a -n $CRITICAL ]
 then
-    echo "Current CPU Utilization is: $CPU_UTILIZATION%"
-    mail -s "High CPU Alert" neelareddy.i10204@gmail.com <<< "CPU usage is above $THRESHOLD%."
+if [ "$PULOAD" -ge "$WARNING" -a "$CPULOAD" -lt "$CRITICAL" ]
+then
+    echo "$TIMESTAMP WARNING - $CPULOAD on Host $HOSTNAME" &>>$LOGFILE
+    echo "Warning Cpuload $CPULOAD Host is $HOSTNAME" | mail -s "CPULOAD is Warning" $WARNINGMAIL
+    exit 1
+elif [ "$CPULOAD" -ge "$CRITICAL" ]
+then
+    echo "$TIMESTAMP CRITICAL- $CPULOAD on Host $HOSTNAME" &>>$LOGFILE
+    echo "CRITICAL Cpuload $CPULOAD Host is $HOSTNAME" | mail -s "CPULOAD is CRITICAL" $CRITICALMAIL
+    exit 2   
+else
+    echo "$TIMESTAMP  OK - $CPULOAD on $HOSTNAME" &>>$LOGFILE
 fi
+fi                 
+done
