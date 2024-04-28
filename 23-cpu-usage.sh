42c5d93c6303764465f6953dd9a1f53c50e241bd
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-28 21:02:53 +0530

    Doing good forever

--- 23-cpu-usage.sh
+++ 23-cpu-usage.sh
@@ -15,4 +15,20 @@ touch $LOGFILE
 for path in $PATH
 do 
     CPULOAD=`top -b -n 5 -d1 | grep "Cpu(s)" | tail -n1 | awk '{print $2}' | awk -F . '{print $1}'`
-    
+if [ -n $WARNING -a -n $CRITICAL ]
+then
+if [ "$PULOAD" -ge "$WARNING" -a "$CPULOAD" -lt "$CRITICAL" ]
+then
+    echo "$TIMESTAMP WARNING - $CPULOAD on Host $HOSTNAME" &>>$LOGFILE
+    echo "Warning Cpuload $CPULOAD Host is $HOSTNAME" | mail -s "CPULOAD is Warning" $WARNINGMAIL
+    exit 1
+elif [ "$CPULOAD" -ge "$CRITICAL" ]
+then
+    echo "$TIMESTAMP CRITICAL- $CPULOAD on Host $HOSTNAME" &>>$LOGFILE
+    echo "CRITICAL Cpuload $CPULOAD Host is $HOSTNAME" | mail -s "CPULOAD is CRITICAL" $CRITICALMAIL
+    exit 2   
+else
+    echo "$TIMESTAMP  OK - $CPULOAD on $HOSTNAME" &>>$LOGFILE
+fi
+fi                 
+done
