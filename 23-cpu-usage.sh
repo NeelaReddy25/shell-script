1ed0a11ee5c110d6219813fd95facee817d2bb3b
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-28 17:51:32 +0530

    Doing good forever

--- 23-cpu-usage.sh
+++ 23-cpu-usage.sh
@@ -15,4 +15,3 @@ then
     mail -s "$SUBJECT" $TO < $MESSAGE
 fi
 
-*/5 * * * * /usr/bin/cat /proc/loadavg | awk '{print $1}' | awk '{ if($1 > 80) printf("Current CPU Utilization is: %.2f%%\n"), $0;}' | mail -s "High CPU Alert" neelareddy.i10204@gmail.com
