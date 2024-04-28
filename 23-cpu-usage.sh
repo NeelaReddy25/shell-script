3066284db4fc306531daeadb7787ede2a99e3c49
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-28 21:23:49 +0530

    Doing good forever

--- 23-cpu-usage.sh
+++ 23-cpu-usage.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
-
-PATH="/"
-HOSTNAME=$(hostname)
-CRITICAL=80
-WARNING=75
-CRITICALMAIL="neelareddy.i25@gmail.com"
-WARNINGMAIL="neelareddy.i10204@gmail.com"
-mkdir -d /var/log/cputilhist
+hostname=$(HOSTNAME)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE=/var/log/cputilhist/cpusage-$TIMESTAMP.log
 
-touch $LOGFILE
+cpuuse=$(cat /proc/loadavg | awk '{print $3}' | cut -f 1 -d ".")
 
-for path in $PATH
-do 
-    CPULOAD=`top -b -n 5 -d1 | grep "Cpu(s)" | tail -n1 | awk '{print $2}' | awk -F . '{print $1}'`
-if [ -n $WARNING -a -n $CRITICAL ]
-then
-if [ "$PULOAD" -ge "$WARNING" -a "$CPULOAD" -lt "$CRITICAL" ]
-then
-    echo "$TIMESTAMP WARNING - $CPULOAD on Host $HOSTNAME" &>>$LOGFILE
-    echo "Warning Cpuload $CPULOAD Host is $HOSTNAME" | mail -s "CPULOAD is Warning" $WARNINGMAIL
-    exit 1
-elif [ "$CPULOAD" -ge "$CRITICAL" ]
-then
-    echo "$TIMESTAMP CRITICAL- $CPULOAD on Host $HOSTNAME" &>>$LOGFILE
-    echo "CRITICAL Cpuload $CPULOAD Host is $HOSTNAME" | mail -s "CPULOAD is CRITICAL" $CRITICALMAIL
-    exit 2   
-else
-    echo "$TIMESTAMP  OK - $CPULOAD on $HOSTNAME" &>>$LOGFILE
+if [ "$cpuuse" -ge 90 ]; then
+    SUBJECT="ATTENTION: CPU load is high on $(hostname) at $(TIMESTAMP)"
+    MESSAGE="/tmp/Mail.out"
+    TO="neelareddy.i25@gmail.com"
+    echo "CPU current usage is: $cpuuse%" >> $MESSAGE
+    echo "" >> $MESSAGE
+    echo "+------------------------------------------------------------------+" >> $MESSAGE
+    echo "Top 20 processes consuming high CPU" >> $MESSAGE
+    ps --sort=-pcpu | head -n 6 >> $MESSAGE
+    mail -s "$SUBJECT" $TO < $MESSAGE
 fi
-fi                 
-done
+
+echo "$MESSAGE" | mail -s "High CPU Alert" neelareddy.i10204@gmail.com
