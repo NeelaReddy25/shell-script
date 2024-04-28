855c837dd2eb5530dc58fa1767be156b08f1993c
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-29 02:04:30 +0530

    Doing good forever

--- 25-rows-columns.sh
+++ 25-rows-columns.sh
@@ -1,16 +1,32 @@
+#!/bin/bash
+
 awk '
 {
-    for (i=1; i<=NF; i++) {
-        a[NR,i] = $i
+  for (i=1; i<=NF; i++) {
+    if(NR==1) {
+      s[i] = $i;
+    } else {
+      s[i] = s[i] " " $i;
     }
+  }
 }
 END {
-    for (i=1; i<=NF; i++) {
-        for (j=1; j<=NR; j++) {
-            printf "%s%s", a[j,i], (j==NR ? "\n" : OFS)
-        }
-    }
+  for (i=1; i<=NF; i++) {
+    print s[i];
+  }
 }' file.txt
 
 
-paste -sd: < file.txt | column -t -s:
+awk '
+{
+  for (i=1; i<=NF; i++) {
+    a[NR,i] = $i
+  }
+}
+END {
+  for (i=1; i<=NF; i++) {
+    for (j=1; j<=NR; j++) {
+      printf "%s%s", a[j,i], (j==NR ? RS : FS)
+    }
+  }
+}' input.txt
