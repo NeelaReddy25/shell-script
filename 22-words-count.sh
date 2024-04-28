3b7c373ba0928ceff87f2abc1e22180d00d47028
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-28 16:03:49 +0530

    Doing good forever

--- 22-words-count.sh
+++ 22-words-count.sh
@@ -11,10 +11,10 @@ then
     exit 1
 fi
 
-# word_count=$(tr -s '[:space:]' '\n' < "$filename" | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | grep -v '^$' | sort | uniq -c | sort -nr)
+word_count=$(tr -s '[:space:]' '\n' < "$filename" | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | grep -v '^$' | sort | uniq -c | sort -nr)
 
-# echo "Top 5 Most Frequent Words:"
-# echo "$word_count" | head -n 5 | awk '{print $2 " - " $1 " occurrences"}'
+echo "Top 5 Most Frequent Words:"
+echo "$word_count" | head -n 5 | awk '{print $2 " - " $1 " occurrences"}'
 
-word=$(echo -n "i am working has devops engineer " | wc -w)
-echo "Number of Words = $word" 
+# word=$(echo -n "i am working has devops engineer " | wc -w)
+# echo "Number of Words = $word" 
